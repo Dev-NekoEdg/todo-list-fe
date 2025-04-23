e49074c126166450d7dacb1adf5c2ac8a4d70ce7
Edgard Alvarado <Edgard-0211@hotmail.com>
2025-04-22 19:46:04 -0500

    tests signals

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  public counter = signal<number>(0);
+  public isValid = computed(()=>{
+    const c = this.counter();
+    if(c > 0 )
+    {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+    
+  );
 
+  restart() {
+    this.counter.set(0);
+  }
+
+  sum() {
+    this.counter.update((c) => c + 1);
+  }
 }
